54638e5d22c38644185afb418de92b89acd75d8e
Runolf <alexandreliskiewicz@hotmail.com>
2020-10-02 17:09:52 +0200

    DAL + basis code to get a list of beer

---
+++ BackBeerProject/BackBeerProject/Controllers/BeerController.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BackBeerProject.Controllers
+{
+    public class BeerController : ApiController
+    {
+        private BeerProjectEntities _context = new BeerProjectEntities();
+        public IEnumerable<Beer> Get()
+        {
+            return _context.Beer.ToList();
+        }
+
+        public Beer Get(int ID)
+        {
+            return _context.Beer.FirstOrDefault(e => e.BeerID == ID);
+        }
+    }
+}
