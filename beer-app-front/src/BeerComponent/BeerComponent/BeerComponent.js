5766feedfa5c5c9d2c6be08fa48cf24c3a184477
Runolf <alexandreliskiewicz@hotmail.com>
2020-10-03 13:54:00 +0200

    better style

--- beer-app-front/src/BeerComponent/BeerComponent/BeerComponent.js
+++ beer-app-front/src/BeerComponent/BeerComponent/BeerComponent.js
@@ -23,37 +23,46 @@ export default class BeerComponent extends Component{
     render(){
 
         var {isLoaded, beers} = this.state;
+
         const style = {
+            position: "relative",
+            left: "20px",
+            top: "100px"
+        }
+
+        const style2 = {
             width: "35rem",
             padding: "2px",
             marginBottom: "10px",
-            position: "relative",
-            top: "50px",
-            left: "30%"
+            color: "#D2691E",
+            border: "1px solid brown"
         }
+
         
         if(!isLoaded){
           return <div> <p>Is not loaded yet</p> </div>
         }
         else{
           return (
-            <div>
+            <div className="row row-cols-1 row-cols-md-3" style={style}>
                 {beers.map(beer => ( 
-                    <div className="card" style={style}>
-                        <div className="card-body txt-card">
-                            <h5 className="card-title">{beer.Name}</h5>
-                            <p className="card-text">Brewery: {beer.Brewery}</p>
-                            <p className="card-text">Country: {beer.Country}</p>
-                            <p className="card-text">Degree: {beer.Degree}</p>
-                            <p className="card-text">Color: {beer.Color}</p>
-                            <p className="card-text">Fermentation: {beer.Fermentation}</p>
-                            {beer.SpecialCat != null?
-                                <p className="card-text">Special Category: {beer.SpecialCat}</p>
-                                :
-                                <p className="card-text">Special Category: no special category</p>
-                            }
-                            
-                        </div>    
+                    <div className="col mb-4">
+                        <div className="card" style={style2}>
+                            <div className="card-body txt-card">
+                                <h5 className="card-title">{beer.Name}</h5>
+                                <p className="card-text">Brewery: {beer.Brewery}</p>
+                                <p className="card-text">Country: {beer.Country}</p>
+                                <p className="card-text">Degree: {beer.Degree}</p>
+                                <p className="card-text">Color: {beer.Color}</p>
+                                <p className="card-text">Fermentation: {beer.Fermentation}</p>
+                                {beer.SpecialCat != null?
+                                    <p className="card-text">Special Category: {beer.SpecialCat}</p>
+                                    :
+                                    <p className="card-text">Special Category: no special category</p>
+                                }
+                                
+                            </div>    
+                        </div>
                     </div>
                 ))}
             </div>
