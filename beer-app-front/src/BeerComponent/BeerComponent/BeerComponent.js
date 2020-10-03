8d62effd5234a4b72f22fdcf0a82d8ea081c572f
Runolf <alexandreliskiewicz@hotmail.com>
2020-10-03 13:09:14 +0200

    add bootstrap + some css

--- beer-app-front/src/BeerComponent/BeerComponent/BeerComponent.js
+++ beer-app-front/src/BeerComponent/BeerComponent/BeerComponent.js
@@ -3,14 +3,13 @@ import React, {Component} from 'react';
 export default class BeerComponent extends Component{
 
     
-          state = {
-            beers:[],
-            isLoaded: false
-          }
-    
+    state = {
+        beers:[],
+        isLoaded: false
+    }
 
 
-      componentDidMount(){
+    componentDidMount(){
         fetch('https://localhost:44369/api/beer')
         .then(res => res.json())
         .then(json => {
@@ -19,24 +18,44 @@ export default class BeerComponent extends Component{
             beers: json
           })
         });
-      }
+    }
 
-      render(){
+    render(){
 
         var {isLoaded, beers} = this.state;
+        const style = {
+            width: "35rem",
+            padding: "2px",
+            marginBottom: "10px",
+            position: "relative",
+            top: "50px",
+            left: "30%"
+        }
+        
         if(!isLoaded){
           return <div> <p>Is not loaded yet</p> </div>
         }
         else{
           return (
-            <div className="App">
-               <ul>
-                 {beers.map(beer => ( 
-                   <li key={beer.id}>
-                     {beer.Name}
-                   </li>
-                 ))}
-               </ul>
+            <div>
+                {beers.map(beer => ( 
+                    <div className="card" style={style}>
+                        <div className="card-body txt-card">
+                            <h5 className="card-title">{beer.Name}</h5>
+                            <p className="card-text">Brewery: {beer.Brewery}</p>
+                            <p className="card-text">Country: {beer.Country}</p>
+                            <p className="card-text">Degree: {beer.Degree}</p>
+                            <p className="card-text">Color: {beer.Color}</p>
+                            <p className="card-text">Fermentation: {beer.Fermentation}</p>
+                            {beer.SpecialCat != null?
+                                <p className="card-text">Special Category: {beer.SpecialCat}</p>
+                                :
+                                <p className="card-text">Special Category: no special category</p>
+                            }
+                            
+                        </div>    
+                    </div>
+                ))}
             </div>
           );
         }
